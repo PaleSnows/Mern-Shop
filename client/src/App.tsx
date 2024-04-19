5d9365df93e58dec10ea47b208a5650e249cf6b7
Renzaki <renzakithakuri@gmail.com>
2024-04-19 16:42:51 +0545

    adding lazy and suspense feature to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Search from "./pages/Search"
-import Cart from "./pages/Cart"
-
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader";
+//using lazy to render only page that is currently being used for more effient usage
+const Home = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/search" element={<Search/>}/>
-      <Route path="/cart" element={<Cart/>}/>
-    </Routes>
+    {/* suspense for creating fallback that shows something before the page is fully loaded */}
+      <Suspense fallback={<Loader/>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
